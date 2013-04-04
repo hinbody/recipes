e0eeeccd23be873f7a6b4933a5286666f42ae6e0
Harley Inbody <hinbody@gmail.com>
2013-04-04 18:34:43 -0400

    Generate recipes as a Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recipes::Application.config.secret_key_base = '1e1c050c361cc46caaa6881aa8aea168ec7873e380fc224aedfae0bbe6a37fec71f7659a9db63ff2845836850466ecb331236afce71fe6d86e022cedd5793f65'
