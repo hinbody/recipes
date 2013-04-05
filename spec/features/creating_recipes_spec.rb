62a97c3ea9f8f437104f32d02c678bf59cd67baa
Harley Inbody <hinbody@gmail.com>
2013-04-04 20:38:36 -0400

    Add validation to require names when creating recipes

--- spec/features/creating_recipes_spec.rb
+++ spec/features/creating_recipes_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 feature 'Creating Recipes' do
-  scenario "can create a recipe" do
+  before do
     visit '/'
 
     click_link 'New Recipe'
+  end
 
+  scenario "can create a recipe" do
     fill_in 'Recipe Name', :with => 'Popcorn'
     fill_in 'Description', :with => 'Making popcorn on the stove'
     click_button 'Create Recipe'
 
     expect(page).to have_content('Recipe has been created.')
+  end
+
+  scenario "can not create a recipe without a name" do
+    click_button 'Create Recipe'
+
+    expect(page).to have_content("Recipe has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end
